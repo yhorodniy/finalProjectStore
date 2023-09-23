228b3e57f54dcd07556b0e8c0a6fafd1af4d71d3
Yurii Horodnii <y.horodniy@gmail.com>
2023-09-24 01:54:05 +0300

    done register form for mobile

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import LoginForm from '../../components/LoginForm/LoginFormTemplate';
 
 const HomePage = () => {
   return (
-    <h1>Home</h1>
+    <LoginForm />
   );
 };
 
